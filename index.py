264393e2dd2cc09853ad7b4b8edb9e76c9f15f4f
Aaron.Ji <aaron.ji@learnosity.com>
2020-01-13 14:19:58 +1100

    get email address

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 # Import libraries
 import requests
+from requests.exceptions import Timeout
 import urllib.request
 import time
 from bs4 import BeautifulSoup
 import xlwt
 from datetime import datetime
 import re
+
+REQUEST_TIMEOUT = 2
 
 def saveToFile(string, file=None):
     if file==None:
@@ -14,16 +17,86 @@ def saveToFile(string, file=None):
     f.write(string)
     f.close()
 
+def getEmailFromContactUs(href):
+    try:
+        response = requests.get(href,timeout= REQUEST_TIMEOUT)
+    except Timeout:
+        print('The reqeust timed out')
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+        contactUri = soup.find('a', string= ["Contact Us", "About Us"])
+        print(contactUri)
+        if contactUri is not None:
+            # if the uri starts with /
+            if contactUri['href'].startswith('/'):
+                url = href + contactUri['href']
+            else:
+                url = contactUri['href']
+            print('url:' + url)
+            try:
+                response = requests.get(url,timeout= REQUEST_TIMEOUT)
+            except Timeout:
+                print('The reqeust timed out')
+            else:
+                soup = BeautifulSoup(response.text, "html.parser")
+                email = soup.find(string=re.compile('^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$'))
+                if email is not None:
+                    print('email------>' + email)
+                    return email
+    return ''
+
+
+def getEmailAddress(href):
+    email = getEmailFromFacebook(href)
+    if email is None or len(email) < 1:
+        email = getEmailFromContactUs(href)
+    return email
+
+def getEmailFromFacebook(href):
+    try:
+        response = requests.get(href,timeout= REQUEST_TIMEOUT)
+    except Timeout:
+        print('The reqeust timed out')
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        aLink = soup.find('a', href=lambda href: href and ('facebook.com' in href))
+        if aLink is None:
+            return ''
+        else:
+            if len(aLink['href']) > 0:
+                if (aLink['href'].endswith('/')):
+                    aboutLink = aLink['href'] + 'about'
+                else:
+                    aboutLink = aLink['href'] + '/about'
+                print(aboutLink)
+                try:
+                    response = requests.get(aboutLink,timeout= REQUEST_TIMEOUT)
+                except Timeout:
+                    print('The reqeust timed out- fetch email')
+                else:
+                    soup = BeautifulSoup(response.text, "html.parser")
+                    emailLink = soup.find('a', href=lambda href: href and ('@' in href))
+                    if emailLink is not None:
+                        email = emailLink.text
+                        print(email)
+                        return email
+
+    return ''
 def getInstagramLink(href):
-    response = requests.get(href,timeout=2)
-    print(response)
-    soup = BeautifulSoup(response.text, "html.parser")
-    aLinks = soup.find('a', href=lambda href: href and ("http://instagram.com" in href or "https://instagram.com" in href))
-    
-    print(aLinks)
-    # for node in aLinks:
-    #     if len(node['href']) > 0:
-    #         return node['href']
+    try:
+        response = requests.get(href,timeout= REQUEST_TIMEOUT)
+    except Timeout:
+        print('The reqeust timed out')
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+        aLink = soup.find('a', href=lambda href: href and ("http://instagram.com" in href or "https://instagram.com" in href))
+        if aLink is None:
+            return ''
+        else:
+            if len(aLink['href']) > 0:
+                return aLink['href']
+
     return ''
 def getUrls(text):
     # Parse HTML and save to BeautifulSoup object¶
@@ -44,9 +117,11 @@ def getUrls(text):
             print(child['href'])
             href = child['href']
             instagramLink = getInstagramLink(href)
+            facebookLink = getEmailAddress(href)
             ws.write(i+1, 0, href)
+            ws.write(i+1, 1, facebookLink)
             ws.write(i+1, 2, instagramLink)
-            wb.save('example.xls')
+    wb.save('example.xls')
 
 baseUrl = 'https://www.shopistores.com/shopify/'
 i = 1
